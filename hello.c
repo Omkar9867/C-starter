eeab507b9552268f3b7fdb8d605dc3cb5d892adb
Omkar <omkarrajan495@gmail.com>
2025-02-04 20:34:03 +0530

    functions

--- hello.c
+++ hello.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+// Functions must exist before they are called.
+// int multiply(int a, int b){
+//     return a*b;
+// }
+
+// Second method is to declare the function and then can define it later
+int square(int x);
+
 int main() {
 
     // puts("Hello, World2!");
@@ -22,10 +30,10 @@ int main() {
     // printf("Marks of student 3 is %d\n", marks[2]);
 
     //Loops
-    int marks[3];
-    marks[0] = 34;
-    marks[1] = 45;
-    marks[2] = 67;
+    // int marks[3];
+    // marks[0] = 34;
+    // marks[1] = 45;
+    // marks[2] = 67;
     //for loop ----->
     // for(int i=0; i<3; i++){
     //     printf("Marks of students marks[%d] is %d\n", i, marks[i]);
@@ -39,15 +47,26 @@ int main() {
     // }
 
     //Block scope
-    int x = 40;
-    {
-        int x = 30;
-        x++;
-        printf("Value of x inside curly braces is %d\n", x); // the value is x in this curly braces only
-    }
-    printf("Value of x outside curly braces is %d\n", x); // so this value of x will not have access to the value of x inside the curly braces
+    // int x = 40;
+    // {
+    //     int x = 30;
+    //     x++;
+    //     printf("Value of x inside curly braces is %d\n", x); // the value is x in this curly braces only
+    // }
+    // printf("Value of x outside curly braces is %d\n", x); // so this value of x will not have access to the value of x inside the curly braces
 
+
+    //Functions
+    // int value = multiply(2, 3);
+    // printf("The value of 2 * 3 is:  %d\n", value);
+
+    printf("The square of 5 is: %d\n", square(5));
     return 0;
+}
+
+//Here i am defining the function
+int square(int x){
+    return x*x;
 }
 
 //Compilation --->
