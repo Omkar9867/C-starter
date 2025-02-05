41ad21974a45bbc3a8fdca88b24a52d049f0261e
Omkar <omkarrajan495@gmail.com>
2025-02-05 22:50:42 +0530

    pointers, pass-by-values

--- hello2.c
+++ hello2.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <assert.h>
+#include <stdlib.h>
 
 //Recurssion
 // void func(int a){
@@ -36,16 +37,54 @@ int main(){
     // }
 
     //Ampercend operator
-    int x = 10;
-    printf("Value of x in memory is: %p\n", &x); //will print the memory value if the actual value, %p is the format specifier for printing the values of pointers
+    // int x = 10;
+    // printf("Value of x in memory is: %p\n", &x); //will print the memory value if the actual value, %p is the format specifier for printing the values of pointers
     // if we want the value from the (&x)
-    printf("Actual value of x is: %d\n", *(&x)); // pointer helps to extract the value stored in the address
+    // printf("Actual value of x is: %d\n", *(&x)); // pointer helps to extract the value stored in the address
 
     //Pointers
     //Creating the pointer
-    int* px = &x; // * is used to create the pointer, and & is used to get the address of the variable
+    // int* px = &x; // * is used to create the pointer, and & is used to get the address of the variable
 
     //Accessing the value to what we pointed to.
-    printf("The value of pointed valu: %d\n", *px);
+    // printf("The value of pointed valu: %d\n", *px);
+
+
+    //Malloc & Free
+    int sizeOfArr = 0;
+    // scanf("%d", &sizeOfArr);
+    int* dynamicArr = malloc(sizeof(int)*sizeOfArr);
+    // dynamicArr[0] = 1;
+    // dynamicArr[1] = 2;
+    // dynamicArr[2] = 3;
+    // dynamicArr[3] = 4;
+    // dynamicArr[4] = 5;
+    // dynamicArr[5] = 6;
+    // dynamicArr[6] = 6;
+    // dynamicArr[7] = 6;
+    // dynamicArr[8] = 6;
+    for (int i = 0; i < sizeOfArr; i++){
+        printf("%d\n", dynamicArr[i]);
+    } 
+    printf("Size of arr is: %d\n", sizeof(dynamicArr));
+    free(dynamicArr);
+    printf("Size of arr is: %d\n", sizeof(dynamicArr));
+    
+    //The reason to use Malloc and Free is because it allows to determine while our program is running how much memory to allocate,
+    //And we use pointer to 'point-to' the dynamically allocated memory.
+
+    //Pass by value
+    void setVal(int* val) {
+        *(val) = 999;
+    };
+    int x = 10;
+    // setVal(x); //When we pass the x to the fucntion it makes a copy of the value of x and pass it to the function, so the value of x will not change
+    // printf("Value of x is: %d\n", x);
+    //but if you want to chnage value:
+    setVal(&x); //passing the address of x, but the function need to access this address by making its value to pointer
+    printf("Value of x is: %d\n", x);
     return 0;
 }
+
+//Malloc [M-Memory, alloc-allocator] ==> Return a pointer to the start of a specific number of bytes. (Note 'sizeof(type)' retrieves the number of bytes of a type)
+//Free ==> Release the memory that was allocated by malloc. (Note: It is important to free the memory that was allocated by malloc to avoid memory leaks)
